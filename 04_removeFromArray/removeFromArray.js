e2d47b264d8183115a0fa79b1c1a724f2b9c19be
Ahmad Abu Salem <ahmad.mei.as@gmail.com>
2022-02-10 01:28:13 +0200

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    for (item of items) {
+        let location = array.indexOf(item);
+        if (location >= 0) {
+            array.splice(location, 1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
