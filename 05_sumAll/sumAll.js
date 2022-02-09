df5e18b92da5f8b44398b09dbd5df0351d339f9f
Ahmad Abu Salem <ahmad.mei.as@gmail.com>
2022-02-10 01:38:04 +0200

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if (firstNumber < 0 || secondNumber < 0 || typeof firstNumber !== "number" || typeof secondNumber !== "number") {
+        return "ERROR";
+    }
 
+    let sum = 0;
+
+    for (let i = Math.min(firstNumber, secondNumber); i <= Math.max(firstNumber, secondNumber); i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
