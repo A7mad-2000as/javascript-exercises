4d68deb6d330489654cc1f9f26888d1e37db656e
Ahmad Abu Salem <ahmad.mei.as@gmail.com>
2022-02-13 21:43:42 +0200

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => oldest === undefined || getAge(current) > getAge(oldest) ? current : oldest);
 };
+
+function getAge(person) {
+    if ("yearOfDeath" in person) {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+
+    else {
+        return (new Date()).getFullYear() - person.yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
