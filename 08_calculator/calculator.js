72bb15e6984258c3c460bd40f57a8b80c2a01733
Ahmad Abu Salem <ahmad.mei.as@gmail.com>
2022-02-13 20:27:40 +0200

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((sum, num) => sum += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((answer, num) => answer *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(firstNum, secondNum) {
+	return firstNum ** secondNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let allNums = [...Array(num).keys()].map(num => num + 1); // all nums from 1 to num
+  return allNums.reduce((answer, num) => answer *= num, 1);
 };
 
 // Do not edit below this line
