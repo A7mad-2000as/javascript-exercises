38397df75930484989034be9763ea15a782c646a
Ahmad Abu Salem <ahmad.mei.as@gmail.com>
2022-02-13 21:00:55 +0200

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    
+    let first = 1;
+    let second = 1;
+    for (let seq = 3; seq <= num; seq++) {
+        let current = first + second;
+        second = first;
+        first = current;
+    }
 
+    return first;
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
