5b73d836be724c9ca2a358632844e80d4137c45b
Ahmad Abu Salem <ahmad.mei.as@gmail.com>
2022-02-13 20:44:31 +0200

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toUpperCase();
+    const charArray = string.split("");
+    const filteredCharArray = charArray.filter(char => char >= "A" && char <= "Z");
+    
+    return filteredCharArray.slice().reverse().join("") === filteredCharArray.join("");
 };
+
+palindromes("ZZZZ car, a man, a maraca.");
 
 // Do not edit below this line
 module.exports = palindromes;
